b49f08ee068ba0a605c8d8651ab9b5d609aa155c
sezgi-yonten <sezgiyontenn@gmail.com>
2024-12-18 08:46:38 +0300

    update the login/register area

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://workintech-fe-ecommerce.onrender.com',
+  baseURL: 'https://workintech-fe-ecommerce.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './styles/form.css';  // Import form styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
